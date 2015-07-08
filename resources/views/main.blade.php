fa89022ff2604da2c6a799b9f11c2fc1670565ab
Kevin MacMartin <prurigro@gmail.com>
2015-07-08 03:34:58 -0400

    Initial commit with a simple working site and the framework tweaked to work around it

---
+++ resources/views/main.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Hypothetical Template</title>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <script src="{{ elixir("js/lib.js") }}"></script>
+        <script src="{{ elixir("js/app.js") }}"></script>
+        @if (Config::get('app.debug'))
+            <script type="text/javascript">
+                document.write('<script src="//localhost:35729/livereload.js?snipver=1" type="text/javascript"><\/script>')
+            </script> 
+        @endif
+        <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+    </head>
+    <body class="{{ Request::path() == "/" ? "index" : Request::path() }}">
+        @yield('page')
+    </body>
+</html>
