859c8987d70265ef102985ae5dfae650b56b501a
Kevin MacMartin <prurigro@gmail.com>
2015-07-11 06:27:21 -0400

    Remove some trailing whitespace in the main blade

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -9,7 +9,7 @@
         @if (Config::get('app.debug'))
             <script type="text/javascript">
                 document.write('<script src="//localhost:35729/livereload.js?snipver=1" type="text/javascript"><\/script>')
-            </script> 
+            </script>
         @endif
         <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
     </head>
