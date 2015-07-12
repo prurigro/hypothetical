c29d899f8258ae65499b3e8a89f4c824cd435f1b
Kevin MacMartin <prurigro@gmail.com>
2015-07-12 19:46:22 -0400

    Swap double quotes wrapped in double quotes for single quotes

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,8 +5,8 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script src="{{ elixir("js/lib.js") }}"></script>
-        <script src="{{ elixir("js/app.js") }}"></script>
+        <script src="{{ elixir('js/lib.js') }}"></script>
+        <script src="{{ elixir('js/app.js') }}"></script>
         @if (Config::get('app.debug'))
             <script type="text/javascript">
                 document.write('<script src="//localhost:35729/livereload.js?snipver=1" type="text/javascript"><\/script>')
