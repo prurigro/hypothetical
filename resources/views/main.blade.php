815b5a2c9835076a9c24c2de18c376c02d11c0fa
Kevin MacMartin <prurigro@gmail.com>
2015-07-12 19:29:42 -0400

    Add the viewport meta tag

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,6 +4,7 @@
         <title>Hypothetical Template</title>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <script src="{{ elixir("js/lib.js") }}"></script>
         <script src="{{ elixir("js/app.js") }}"></script>
         @if (Config::get('app.debug'))
