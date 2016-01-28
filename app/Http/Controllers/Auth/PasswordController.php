336de1cedb4a8fa832bb3f4ba17a1c69de956ea1
Kevin MacMartin <prurigro@gmail.com>
2016-01-27 22:19:27 -0500

    Redirect to /dashboard after the password has been reset

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -21,6 +21,13 @@ class PasswordController extends Controller
     use ResetsPasswords;
 
     /**
+     * Where to redirect users after login / registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/dashboard';
+
+    /**
      * Create a new password controller instance.
      *
      * @return void
