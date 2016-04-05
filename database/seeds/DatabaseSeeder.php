16b08b7ee1b0e975f7cfb9d9c4f2af44a46d3985
Kevin MacMartin <prurigro@gmail.com>
2016-04-05 14:27:59 -0400

    Unguard and reguard the database during the seeding process

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-
+        Model::unguard();
+        Model::reguard();
     }
 }
