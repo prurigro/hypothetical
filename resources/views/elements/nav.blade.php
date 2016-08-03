ab1e828421488f5001d216626e0b5bf106a3488f
Kevin MacMartin <prurigro@gmail.com>
2016-08-02 22:55:30 -0400

    Formatting in the nav

--- resources/views/elements/nav.blade.php
+++ resources/views/elements/nav.blade.php
@@ -7,8 +7,10 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
+
             <a class="navbar-logo" href="/"></a>
         </div>
+
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
                 <li class="navlink"><a href="/" title="Home">Home</a></li>
