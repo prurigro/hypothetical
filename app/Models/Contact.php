2d9e500ebe068640d09f2dc325ce33f0252dc8a5
Kevin MacMartin <prurigro@gmail.com>
2016-09-03 00:30:02 -0400

    Format Models like the updated User model

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -1,16 +1,25 @@
-<?php namespace App\Models;
+<?php
+
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Contact extends Model {
-
-    // The contact table
+class Contact extends Model
+{
+    /**
+     * The contact table
+     *
+     * @var string
+     */
     protected $table = 'contact';
 
-    // Returns the list of all contact submissions
+    /**
+     * Returns the list of all contact submissions
+     *
+     * @return array
+     */
     public static function getContactSubmissions()
     {
         return self::orderBy('created_at', 'desc')->get();
     }
-
 }
--- app/Models/Subscriptions.php
+++ app/Models/Subscriptions.php
@@ -1,16 +1,25 @@
-<?php namespace App\Models;
+<?php
+
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Subscriptions extends Model {
-
-    // The subscriptions table
+class Subscriptions extends Model
+{
+    /**
+     * The subscriptions table
+     *
+     * @var string
+     */
     protected $table = 'subscriptions';
 
-    // Returns the list of all subscriptions
+    /**
+     * Returns the list of all subscriptions
+     *
+     * @return array
+     */
     public static function getSubscriptions()
     {
         return self::orderBy('created_at', 'desc')->get();
     }
-
 }
