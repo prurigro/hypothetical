bd1f0ef4df8159f7e89865e433ff8f8649fdadf3
Kevin MacMartin <prurigro@gmail.com>
2016-09-14 11:23:45 -0400

    Pull in latest upstream laravel updates

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => env('SITE_NAME', 'My Application'),
+    'name' => env('SITE_NAME', 'Laravel'),
 
     /*
     |--------------------------------------------------------------------------
