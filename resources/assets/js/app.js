073e9ddf4f519cc7b61eddea8847ff72d13f5021
Kevin MacMartin <prurigro@gmail.com>
2016-10-12 21:34:31 -0400

    Use $(window).on("load" instead of $(window).load as this no longer exists in jquery 3

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,7 +1,7 @@
 // run once the document is ready
 $(document).ready(function() {
     $("footer").stickyFooter({ content: "#page-content" });
-    $(window).load(function() { $(this).trigger("resize"); });
+    $(window).on("load", function() { $(this).trigger("resize"); });
 
     switch (SiteVars.page) {
         case "":
