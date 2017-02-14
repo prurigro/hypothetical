d727ec9d18fa5175e65edf49fd769d6a1ccc1a8a
Kevin MacMartin <prurigro@gmail.com>
2017-02-13 22:42:12 -0500

    Set a mobile chrome theme colour

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -9,6 +9,7 @@
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#fcfcfc" />
 
         <meta name="title" content="{{ $page_title }}" />
         <meta name="description" content="{{ env('SITE_DESC') }}" />
