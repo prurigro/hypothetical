e70dda2e6c01d56025f5be56555010aab005f6ab
Kevin MacMartin <prurigro@gmail.com>
2017-03-06 17:34:55 -0500

    Pull in fix for laravel 5.4 migrations when using older versions of mysql and mariadb

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +15,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Fix for migrations on older versions of mysql and mariadb
+        Schema::defaultStringLength(191);
+
+        // Add the lang blade directive for multi-language support
         Blade::directive('lang', function($expression) {
             return "<?php echo Language::select($expression); ?>";
         });
