45e84497f1144de23dc20d756a9f573ee16c332b
Kevin MacMartin <prurigro@gmail.com>
2018-01-21 22:47:59 -0500

    Cut text off before the buttons in the dashboard edit list instead of having it fall on the next line

--- resources/views/dashboard/edit-list.blade.php
+++ resources/views/dashboard/edit-list.blade.php
@@ -20,43 +20,39 @@
 
         <ul id="edit-list" class="list-group edit-list list" data-model="{{ $model }}" data-path="{{ isset($path) ? $path : $model }}" {{ $sortcol != false ? "data-sort=$sortcol" : '' }}>
             @foreach($rows as $row)
-                <li class="list-group-item" data-id="{{ $row['id'] }}">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="title-column">
-                                @if($sortcol != false)
-                                    <div class="sort-icon">
-                                        <i class="fa fa-bars" title="Click and drag to reorder"></i>
-                                    </div>
-                                @endif
-
-                                @if(is_array($column))
-                                    @foreach($column as $col)
-                                        @if($row[$col] != '')
-                                            <div class="column">{{ $row[$col] }}</div>
-
-                                            @if(!$loop->last)
-                                                <div class="spacer">|</div>
-                                            @endif
-                                        @endif
-                                    @endforeach
-                                @else
-                                    {{ $row[$column] }}
-                                @endif
+                <li class="list-group-item {{ $sortcol != false ? 'sortable' : '' }}" data-id="{{ $row['id'] }}">
+                    <div class="title-column">
+                        @if($sortcol != false)
+                            <div class="sort-icon">
+                                <i class="fa fa-bars" title="Click and drag to reorder"></i>
                             </div>
+                        @endif
 
-                            <div class="button-column">
-                                @if(isset($button) && is_array($button))
-                                    <button type="button" class="action-button btn btn-default" data-confirmation="{{ $button[1] }}" data-success="{{ $button[2] }}" data-error="{{ $button[3] }}" data-url="{{ $button[4] }}">{{ $button[0] }}</button>
+                        @if(is_array($column))
+                            @foreach($column as $col)
+                                @if($row[$col] != '')
+                                    <div class="column">{{ $row[$col] }}</div>
+
+                                    @if(!$loop->last)
+                                        <div class="spacer">|</div>
+                                    @endif
                                 @endif
+                            @endforeach
+                        @else
+                            {{ $row[$column] }}
+                        @endif
+                    </div>
 
-                                <button type="button" class="edit-button btn btn-warning">Edit</button>
+                    <div class="button-column">
+                        @if(isset($button) && is_array($button))
+                            <button type="button" class="action-button btn btn-default" data-confirmation="{{ $button[1] }}" data-success="{{ $button[2] }}" data-error="{{ $button[3] }}" data-url="{{ $button[4] }}">{{ $button[0] }}</button>
+                        @endif
 
-                                @if($delete)
-                                    <button type="button" class="delete-button btn btn-danger">Delete</button>
-                                @endif
-                            </div>
-                        </div>
+                        <button type="button" class="edit-button btn btn-warning">Edit</button>
+
+                        @if($delete)
+                            <button type="button" class="delete-button btn btn-danger">Delete</button>
+                        @endif
                     </div>
                 </li>
             @endforeach
