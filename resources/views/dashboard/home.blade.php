5c1e1ef14e15a83443fcd0e98828e7c1e1687fb3
Kevin MacMartin <prurigro@gmail.com>
2018-04-02 22:47:09 -0400

    Make the dashboard home menu links take up the full column

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -6,21 +6,21 @@
             @if(is_array($menu_item[1]))
                 @foreach($menu_item[1] as $submenu_item)
                     <li class="list-group-item">
+                        {{ $menu_item[0] }}: {{ $submenu_item[0] }}
+
                         <a
                             class="list-group-item-link"
                             href="{{ url('/dashboard/' . $submenu_item[1]) }}">
-
-                            {{ $menu_item[0] }}: {{ $submenu_item[0] }}
                         </a>
                     </li>
                 @endforeach
             @else
                 <li class="list-group-item">
+                    {{ $menu_item[0] }}
+
                     <a
                         class="list-group-item-link"
                         href="{{ url('/dashboard/' . $menu_item[1]) }}">
-
-                        {{ $menu_item[0] }}
                     </a>
                 </li>
             @endif
