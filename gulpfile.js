679ef84d55565f2ccbc8366edd3eee70c3f38dd0
Kevin MacMartin <prurigro@gmail.com>
2018-04-18 23:00:05 -0400

    Add the bootstrap dependency popper.js to the dashboard libs

--- gulpfile.js
+++ gulpfile.js
@@ -58,6 +58,7 @@ const jsDashboard = [
 // Javascript libraries for the dashboard
 const jsDashboardLibs = [
     "node_modules/jquery/dist/jquery.js",
+    "node_modules/popper.js/dist/umd/popper.js",
     "node_modules/bootstrap/dist/js/bootstrap.js",
     "node_modules/pickadate/lib/picker.js",
     "node_modules/pickadate/lib/picker.date.js",
