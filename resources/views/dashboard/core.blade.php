af9c6df63650133deec4608eb9c9b4a44dae57a9
Kevin MacMartin <prurigro@gmail.com>
2018-06-18 16:50:50 -0400

    Improve the dashboard alert and confirmation modal styles

--- resources/views/dashboard/core.blade.php
+++ resources/views/dashboard/core.blade.php
@@ -21,35 +21,19 @@
         </div>
     </div>
 
-    <div id="confirmation-modal" class="modal">
-        <div class="modal-container">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12 col-md-6 offset-md-3 col-lg-4 offset-lg-4">
-                        <div class="card">
-                            <div class="card-header"></div>
-                            <button type="button" class="cancel-button btn btn-primary">Cancel</button>
-                            <button type="button" class="confirm-button btn btn-danger">Confirm</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+    <div id="confirmation-modal" class="modal confirmation">
+        <div class="card">
+            <div class="card-header"></div>
+            <button type="button" class="cancel-button btn btn-primary">Cancel</button>
+            <button type="button" class="confirm-button btn btn-danger">Confirm</button>
         </div>
     </div>
 
-    <div id="alert-modal" class="modal">
-        <div class="modal-container">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12 col-md-6 offset-md-3 col-lg-4 offset-lg-4">
-                        <div class="card">
-                            <div class="card-header">ALERT</div>
-                            <div class="message"></div>
-                            <button type="button" class="accept-button btn btn-primary">OK</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+    <div id="alert-modal" class="modal alert">
+        <div class="card">
+            <div class="card-header">ALERT</div>
+            <div class="message"></div>
+            <button type="button" class="accept-button btn btn-primary">OK</button>
         </div>
     </div>
 @endsection
