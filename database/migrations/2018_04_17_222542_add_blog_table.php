2a88427c2f617bd264e350484ab7aa9af235f712
Kevin MacMartin <prurigro@gmail.com>
2019-04-17 17:29:40 -0400

    Update the blog foreign key so it uses a bigInteger to match the use of bigIncrements for users

--- database/migrations/2018_04_17_222542_add_blog_table.php
+++ database/migrations/2018_04_17_222542_add_blog_table.php
@@ -15,7 +15,7 @@ class AddBlogTable extends Migration
     {
         Schema::create('blog', function(Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('title')->nullable();
             $table->text('body')->nullable();
