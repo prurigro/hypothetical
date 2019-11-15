8b6f1e5111c75b86a47af136a6c15df33a89c970
Kevin MacMartin <prurigro@gmail.com>
2019-11-14 21:07:47 -0500

    Remove the bootstrap javascript from the public js as it makes more sense to include as needed

--- gulpfile.js
+++ gulpfile.js
@@ -46,8 +46,6 @@ const jsPublic = "resources/assets/js/app.js";
 // Javascript libraries for the public site
 const jsPublicLibs = [
     "node_modules/jquery/dist/jquery.js",
-    "node_modules/popper.js/dist/umd/popper.js",
-    "node_modules/bootstrap/dist/js/bootstrap.js",
     "node_modules/gsap/src/uncompressed/TweenMax.js",
     "node_modules/what-input/dist/what-input.js"
 ];
--- traditional-bootstrap/gulpfile.js
+++ traditional-bootstrap/gulpfile.js
@@ -44,8 +44,6 @@ const jsPublic = [
 // Javascript libraries for the public site
 const jsPublicLibs = [
     "node_modules/jquery/dist/jquery.js",
-    "node_modules/popper.js/dist/umd/popper.js",
-    "node_modules/bootstrap/dist/js/bootstrap.js",
     "node_modules/gsap/src/uncompressed/TweenMax.js",
     "node_modules/what-input/dist/what-input.js"
 ];
