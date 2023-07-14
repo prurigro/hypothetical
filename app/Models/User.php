8e3cc32e77c30d713fd9737b586323dbfb020517
Kevin MacMartin <prurigro@gmail.com>
2023-07-14 16:40:08 -0400

    Revert "Revert the User model password hash cast as this breaks things"
    This reverts commit 340f3e1f2319a06a45997fc259b608dab2677a77.

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,7 +44,8 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime'
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
     ];
 
     /**
