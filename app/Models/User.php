bfcc9b526a3f7390bd50be83082d2f774d61f097
Kevin MacMartin <prurigro@gmail.com>
2025-04-30 22:22:02 -0400

    Upgrade to laravel 12.0.7

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,13 +12,14 @@ use App\Traits\Timestamp;
 
 class User extends Authenticatable
 {
+    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasApiTokens, HasFactory, Notifiable;
     use Timestamp;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var list<string>
      */
     protected $fillable = [
         'name',
@@ -30,7 +31,7 @@ class User extends Authenticatable
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var array<int, string>
+     * @var list<string>
      */
     protected $hidden = [
         'password',
